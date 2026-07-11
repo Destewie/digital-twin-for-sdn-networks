940073e2b0953b17c3780ca1f953397fe5454ec7
F <->
2026-07-11 16:27:46 +0200

    prova creazione automatica di topologia custom altra

--- custom_topologies/two_nets.py
+++ custom_topologies/two_nets.py
@@ -1,20 +1,22 @@
 from mininet.topo import Topo
 
-class VLANTopo(Topo):
+class TwoNetsTopo(Topo):
     def build(self):
-        # Add a single switch
+        # Switch for Network 1
         s1 = self.addSwitch('s1')
+        # Switch for Network 2
+        s2 = self.addSwitch('s2')
 
-        # Add hosts and assign them to different VLANs
+        # Hosts on Network 1 (subnet 10.0.1.0/24)
         h1 = self.addHost('h1', ip='10.0.1.1/24')
         h2 = self.addHost('h2', ip='10.0.1.2/24')
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+
+        # Hosts on Network 2 (subnet 10.0.2.0/24)
         h3 = self.addHost('h3', ip='10.0.2.1/24')
         h4 = self.addHost('h4', ip='10.0.2.2/24')
+        self.addLink(h3, s2)
+        self.addLink(h4, s2)
 
-        # Connect hosts to the switch with specific VLAN tags
-        self.addLink(h1, s1, port1=1, port2=1)
-        self.addLink(h2, s1, port1=1, port2=2)  # Same VLAN as h1
-        self.addLink(h3, s1, port1=2, port2=3)  # Different VLAN
-        self.addLink(h4, s1, port1=2, port2=4)  # Same VLAN as h3
-
-topos = { 'vlan': (lambda: VLANTopo()) }
+topos = { 'twonets': (lambda: TwoNetsTopo()) }
