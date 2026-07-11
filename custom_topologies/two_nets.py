047f35d9d50961f6ae865f00cc1330b1f4096a1f
F <->
2026-07-11 16:29:31 +0200

    aggiunta automatica mac

--- custom_topologies/two_nets.py
+++ custom_topologies/two_nets.py
@@ -8,14 +8,14 @@ class TwoNetsTopo(Topo):
         s2 = self.addSwitch('s2')
 
         # Hosts on Network 1 (subnet 10.0.1.0/24)
-        h1 = self.addHost('h1', ip='10.0.1.1/24')
-        h2 = self.addHost('h2', ip='10.0.1.2/24')
+        h1 = self.addHost('h1', ip='10.0.1.1/24', mac='00:00:00:00:00:01')
+        h2 = self.addHost('h2', ip='10.0.1.2/24', mac='00:00:00:00:00:02')
         self.addLink(h1, s1)
         self.addLink(h2, s1)
 
         # Hosts on Network 2 (subnet 10.0.2.0/24)
-        h3 = self.addHost('h3', ip='10.0.2.1/24')
-        h4 = self.addHost('h4', ip='10.0.2.2/24')
+        h3 = self.addHost('h3', ip='10.0.2.1/24', mac='00:00:00:00:00:03')
+        h4 = self.addHost('h4', ip='10.0.2.2/24', mac='00:00:00:00:00:04')
         self.addLink(h3, s2)
         self.addLink(h4, s2)
 
